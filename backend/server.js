b08cd9c169aaee42ea93c6709d6ea74b93f2384d
neiljohnP <neiljohn.parco@tup.edu.ph>
2023-11-22 22:07:33 +0800

    product.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-const app = require('./app')
+const app = require('./app');
+const connectDatabase = require('./config/database.js')
 const PORT = 8080;
 const dotenv = require('dotenv');
 
 //setting up config file
 
-dotenv.config({ path:'./config/config.env'})
+dotenv.config({ path: 'backend/config/config.env'})
+
+
+//connecting to database
+connectDatabase();
 
 app.listen(PORT, () => {
     console.log(`Server started on PORT: ${PORT}`);
